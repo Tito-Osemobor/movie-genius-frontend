774e7bb4f785c73987f939aa17707b09b9b765dc
Tito-Osemobor <titoosemobor@gmail.com>
2024-03-02 23:26:08 -0500

    Create posterData to consume movie api

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,30 +1,37 @@
-import React, {useEffect, useState} from "react";
-import  "./Slider.css";
+import React, { useEffect, useState } from "react";
 import movieApi from "../../service/movieApi";
+import "./Slider.css";
 
 const Slider = () => {
-  const [ posterPaths, setPosterPaths ] = useState([]);
+  const [posterData, setPosterData] = useState([]);
 
   useEffect(() => {
-    movieApi.getRandomPosters()
-      .then(posters => setPosterPaths(posters));
+    movieApi.getRandomPosters().then((posterPaths) => {
+      setPosterData([
+        posterPaths.slice(0, 7),
+        posterPaths.slice(7, 14),
+        posterPaths.slice(14, 21),
+        posterPaths.slice(21, 28),
+      ]);
+    });
   }, []);
 
   return (
-    <div className={"slider"}>
-      <div className="scroll-holder">
-      {divsData.map((divData) => (
-          <div className="scroll-tray">
-            {divData.map((path, index) => (
-              <div>
-                <img key={index} src={path}/>
-              </div>
-            ))}
-          </div>
+    <div className="absolute -top-36">
+      <div className="gallery rotate-12 select-none">
+      {posterData.map((rowData) => (
+        <div className="gallery-tray">
+          {rowData.map((path, index) => (
+            <div>
+              <img key={index} src={path} />
+            </div>
+          ))}
+        </div>
       ))}
-      </div>
     </div>
-  )
-}
+    </div>
+    
+  );
+};
 
 export default Slider;
