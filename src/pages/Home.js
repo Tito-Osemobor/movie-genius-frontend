ff04e0df92a5b656cb0e64f33be5e07ee667f526
Tito-Osemobor <titoosemobor@gmail.com>
2024-03-02 23:26:54 -0500

    Indentation change & Add Slider component background

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,38 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import "../assets/styles/Home.css";
+import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 import Input from "../components/common/Input";
 import Button from "../components/common/Button";
+import Slider from "../components/Slider/Slider";
+import "../assets/styles/Home.css";
 
 const Home = () => {
   return (
-    <div className={"flex flex-col h-screen"}>
-
-      <nav className={"flex items-center justify-between p-5"}>
+    <div className={"flex flex-col h-screen relative"}>
+      <nav className={"flex items-center justify-between p-5 z-50"}>
         <h1 className={"text-red-600 font-extrabold italic text-4xl"}>
           MovieGenius
         </h1>
         <div>
-          <ul>
-            <li></li>
-            <li className={"italic"}>
-              <Link to={"/login"} className={"bg-red-600 py-2 px-4 rounded-md text-white text-md font-bold"}>
-                Sign In
-              </Link>
-            </li>
-          </ul>
+          <Link
+            to={"/login"}
+            className={
+              "bg-red-600 py-2 px-4 rounded-md text-white text-md font-bold italic"
+            }
+          >
+            Sign In
+          </Link>
         </div>
-       </nav>
+      </nav>
 
       <main className={"flex-1 flex items-center justify-center text-center"}>
-        <div className={"space-y-4"}>
+        <Slider />
+        <div className={"space-y-4 z-50"}>
           <div className={"space-y-5"}>
             <h1 className={"text-white font-black text-5xl italic"}>
               Find all the latest movie trailers
             </h1>
-            <p className={"text-white text-2xl"}>From your favourite genres.</p>
+            <p className={"text-white text-2xl"}>From your favorite genres.</p>
           </div>
           <div className={"space-y-4"}>
             <div>
@@ -44,7 +45,11 @@ const Home = () => {
                 <div>
                   <Input placeholder={"Email address"} />
                 </div>
-                <Button text={"Get Started"} textSize={"text-2xl"} iconSize={25}/>
+                <Button
+                  text={"Get Started"}
+                  textSize={"text-2xl"}
+                  iconSize={25}
+                />
               </div>
             </form>
           </div>
@@ -57,4 +62,3 @@ const Home = () => {
 };
 
 export default Home;
-
