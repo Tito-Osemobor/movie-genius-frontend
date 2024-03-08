14b66f4b44306c94e9ea6cd3c67180d74a404af2
Tito-Osemobor <titoosemobor@gmail.com>
2024-03-07 23:18:01 -0500

    Use component for reusability

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,6 +4,7 @@ import Footer from "../components/Footer";
 import Input from "../components/common/Input";
 import Button from "../components/common/Button";
 import Gallery from "../components/Gallery/Gallery";
+import CheckBox from "../components/common/CheckBox/CheckBox";
 import "../assets/styles/Login.css";
 
 const Login = () => {
@@ -20,7 +21,7 @@ const Login = () => {
       </nav>
 
       <main className={"flex-1 flex items-center justify-center px-6"}>
-        <div className={"z-10 bg-opacBlack-70"}>
+        <div className={"z-10 bg-opacBlack-60"}>
           <div className={"space-y-3 px-20 opacity-100 pt-16 pb-36 z-50"}>
             <h2 className={"text-white z-70 text-4xl font-bold"}>Sign In</h2>
             <div className={"flex items-center justify-center"}>
@@ -37,21 +38,7 @@ const Login = () => {
                       />
                     </div>
                   </div>
-                  <div className={"items-center"}>
-                    <input
-                      id="rememberMe"
-                      type="checkbox"
-                      value=""
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300
-                                rounded focus:ring-blue-500 focus:ring-2"
-                    />
-                    <label
-                      htmlFor="rememberMe"
-                      className="ms-2 text-lg font-medium text-white "
-                    >
-                      Remember me
-                    </label>
-                  </div>
+                  <CheckBox />
                   <div>
                     <p className="text-gray-500">
                       New to MovieGenius?{" "}
