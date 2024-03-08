86b32159894dd1925cc6478c877f77ba4836ed60
Tito-Osemobor <titoosemobor@gmail.com>
2024-03-07 23:14:50 -0500

    Change tailwind extended class name

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+  ],
   theme: {
     extend: {
       backgroundColor: {
-        'opacBlack-70': 'rgba(0, 0, 0, 0.7)',
-      }
+        "opacBlack-60": "rgba(0, 0, 0, 0.6)",
+      },
     },
   },
-  plugins: [],
-}
+  plugins: [
+
+  ],
+};
