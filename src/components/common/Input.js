98cb77d7b56eb30b16b8373915add7b6145c3f4a
Tito-Osemobor <titoosemobor@gmail.com>
2024-03-07 23:16:08 -0500

    Proper javascript indentation

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -3,25 +3,30 @@ import React from "react";
 const Input = (props) => {
   return (
     <div className="relative">
-      <input type="text" id={props.id}
-             className="block px-3 pb-2.5
+      <input
+        type="text"
+        id={props.id}
+        className="block px-3 pb-2.5
                         pt-4 w-96 h-16 text-sm text-white
-                        bg-opacBlack-70 rounded-md border border-gray-700
+                        bg-opacBlack-60 rounded-md border border-gray-700
                         appearance-none duration-300 peer
                         focus:ring-0 focus:outline-2 focus:outline-gray-100 focus:outline-offset-4"
-             placeholder=" "/>
-      <label htmlFor={props.id}
-             className="absolute text-sm text-gray-300 font-semibold bg-none
+        placeholder=""
+      />
+      <label
+        htmlFor={props.id}
+        className="absolute text-sm text-gray-300 font-semibold bg-none
                         duration-300 transform -translate-y-4 scale-75 top-5
                         origin-[0] px-3 select-none
                         peer-placeholder-shown:scale-100
                         peer-placeholder-shown:-translate-y-3 peer-placeholder-shown:top-1/2
                         peer-focus:top-5 peer-focus:scale-75 peer-focus:-translate-y-4
-                        start-1 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
+                        start-1 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+      >
         {props.placeholder}
       </label>
     </div>
-  )
-}
+  );
+};
 
 export default Input;
