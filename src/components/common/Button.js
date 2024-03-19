169a06a607a170c9d404634f581f1b0f15ab538c
Tito-Osemobor <titoosemobor@gmail.com>
2024-03-19 00:44:21 -0400

    Adjust button component

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,33 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 const Button = (props) => {
-  return (
-    <div className={"items-center"}>
-      <button
-        className={`flex flex-row items-center justify-center bg-red-600
+  if (props.to) {
+    return (
+      <Link to={props.to}>
+        <div className="items-center">
+          <button
+            className={`flex flex-row items-center justify-center bg-red-600
                     group active:bg-red-700 hover:bg-red-700 rounded-md
-                    py-2 px-6 text-white font-bold
-                    ${props.width} ${props.height}`}>
-        <p className={`group-active:text-gray-300 ${props.textSize}`}>{props.text}</p>
-        <div>
-          <ArrowForwardIcon className={"group-active:text-gray-300"} sx={{
-            fontSize: props.iconSize,
-            fontWeight: 'bold',
-            color: 'white',
-            lineHeight: 28,
-            marginLeft: 1,
-          }}/>
+                    py-2 px-6 text-white font-bold ${props.className}`}>
+            <p className={`group-active:text-gray-300 ${props.textSize}`}>{props.text}</p>
+            <div>
+              <ArrowForwardIcon className={"group-active:text-gray-300"} sx={{
+                fontSize: props.iconSize,
+                fontWeight: 'bold',
+                color: 'white',
+                lineHeight: 28,
+                marginLeft: 1,
+              }}/>
+            </div>
+          </button>
         </div>
-      </button>
-    </div>
-  )
+      </Link>
+    )
+  }
+  else {
+    return (
+      <div className="items-center">
+        <button
+          className={`flex flex-row items-center justify-center bg-red-600
+                    group active:bg-red-700 hover:bg-red-700 rounded-md
+                    py-2 px-6 text-white font-bold ${props.className}`}>
+          <p className={`group-active:text-gray-300 ${props.textSize}`}>{props.text}</p>
+        </button>
+      </div>
+    )
+  }
 }
 
 Button.defaultProps = {
-  textSize: "text-xl",
+  className: "text-xl",
   iconSize: 20,
-  height: "h-16"
 };
 
 export default Button;
