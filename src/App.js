a8d3f0eae42a0c74fee72a3db9e60160a2360310
Tito-Osemobor <titoosemobor@gmail.com>
2024-04-06 23:54:23 -0400

    Implement AuthProvider and private routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import {AuthProvider, BackgroundImageProvider} from "./hooks/useContext/context";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Browse from "./pages/Browse";
-import { BackgroundImageProvider } from "./hooks/useContext/context";
+import SignUp from "./pages/SignUp";
+import PrivateRoute from "./router/PrivateRoute";
 
 const App = () => {
   return (
-    <BackgroundImageProvider>
-      <Routes>
-        <Route path="/" Component={Home} />
-        <Route path="/login" Component={Login} />
-        <Route path="/browse" Component={Browse}/>
-      </Routes>
-    </BackgroundImageProvider>
+    <AuthProvider>
+      <BackgroundImageProvider>
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/login" Component={Login} />
+          <Route path="/signup" Component={SignUp}/>
+          <Route element={<PrivateRoute />}>
+            <Route path="/browse" Component={Browse}/>
+          </Route>
+        </Routes>
+      </BackgroundImageProvider>
+    </AuthProvider>
   );
 }
 
