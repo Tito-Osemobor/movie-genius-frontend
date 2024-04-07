5ba8ea36961ea64b20e9cf57ef24a24187f01aa4
Tito-Osemobor <titoosemobor@gmail.com>
2024-04-06 23:54:52 -0400

    Create authentication context

--- src/hooks/useContext/context.js
+++ src/hooks/useContext/context.js
@@ -1,5 +1,41 @@
-import { createContext, useState, useEffect } from "react";
+import {createContext, useState, useEffect, useContext} from "react";
+import {useNavigate} from "react-router-dom";
 import movieApi from "../../service/movieApi";
+import auth from "../../service/auth";
+
+export const AuthContext = createContext(undefined);
+
+export const AuthProvider = ({children}) => {
+  const [token, setToken] = useState(localStorage.getItem("movieGenius") || "");
+  const navigate = useNavigate();
+
+  const loginAction = async (data) => {
+    try {
+      const response = await auth.login(data);
+      const res = await response.json();
+
+      if (res.token) {
+        setToken(res.token);
+        localStorage.setItem("movieGenius", res.token);
+        navigate("/browse");
+        return;
+      }
+      throw new Error(res.message);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ token }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
 
 export const BackgroundImageContext = createContext(undefined);
 
@@ -23,3 +59,7 @@ export const BackgroundImageProvider = ({ children}) => {
     </BackgroundImageContext.Provider>
   );
 }
+
+export const useBackgroundImage = () => {
+  return useContext(BackgroundImageContext);
+};
