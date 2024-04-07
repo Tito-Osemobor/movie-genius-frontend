910b7b1825d7f00aad4a2eb29360eced0ec39ec5
Tito-Osemobor <titoosemobor@gmail.com>
2024-04-06 23:54:37 -0400

    Add login auth method

--- src/service/auth.js
+++ src/service/auth.js
@@ -3,6 +3,15 @@ import fetchApi from "./fetch";
 const BASE_URL = process.env.REACT_APP_BASE_URL + "/api/v1/auth";
 
 const auth = {
+  login : async (data) => {
+    return fetchApi(`${BASE_URL}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+  },
   userExists : async (email) => {
     return fetchApi(`${BASE_URL}/exists`,{
       headers: {
